e437228ddf97fc2bff6368ffb7f3d04220463693
stas.bucik <stas.bucik@luxonis.com>
2026-04-10 15:30:55 +0200

    debug
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

--- tests/src/onhost_tests/utility/fsync_ptp_test_utils.cpp
+++ tests/src/onhost_tests/utility/fsync_ptp_test_utils.cpp
@@ -427,12 +427,12 @@ int testFsync(float targetFps, struct TestThresholds thresholds) {
 
                 std::cout << "Frame " << name << "\n";
                 std::cout << "\t Seqnum: " << frame->getSequenceNum() << "\n";
-                std::cout << "\t Timestamp: " << std::to_string(frame->getTimestampSystem(dai::CameraExposureOffset::END).value().time_since_epoch().count()) << "\n";
+                std::cout << "\t Timestamp: " << std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(frame->getTimestampSystem(dai::CameraExposureOffset::END).value().time_since_epoch()).count()) << "\n";
 
                 if (prevFrameInfos.find(name) != prevFrameInfos.end()) {
                     auto prevFrameInfo = prevFrameInfos.at(name);
                     std::cout << "\t DIFF Seqnum: " << frame->getSequenceNum() - prevFrameInfo.seqNum << "\n";
-                    std::cout << "\t DIFF Timestamp: " << std::to_string(frame->getTimestampSystem(dai::CameraExposureOffset::END).value().time_since_epoch().count() - prevFrameInfo.ts.time_since_epoch().count()) << "\n";
+                    std::cout << "\t DIFF Timestamp: " << std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(frame->getTimestampSystem(dai::CameraExposureOffset::END).value().time_since_epoch()).count() - td::chrono::duration_cast<std::chrono::microseconds>(prevFrameInfo.ts.time_since_epoch()).count()) << "\n";
                     prevFrameInfos.at(name) = FrameInfo{frame->getTimestampSystem(dai::CameraExposureOffset::END).value(), frame->getSequenceNum()};
                 } else {
                     prevFrameInfos.emplace(name, FrameInfo{frame->getTimestampSystem(dai::CameraExposureOffset::END).value(), frame->getSequenceNum()});
